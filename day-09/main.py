70604cf997f044a29a607b42871528a3ad85ad05
Fahim <mainulislamfahim017803@gmail.com>
2025-12-31 00:22:37 +1100

    day-09

---
+++ day-09/main.py
@@ -0,0 +1,27 @@
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def analysis_log():
+    info = 0
+    warning = 0
+    error = 0
+
+    with open("input.txt", "r") as s:
+        for line in s:
+            info += line.count("INFO")
+            warning += line.count("WARNING")
+            error += line.count("ERROR")
+
+    return {"INFO": info, "WARNING": warning, "ERROR": error}
+
+
+@app.get("/fahim")
+def fahim():
+    
+    result=analysis_log()
+    with open("output.json", "w") as f:
+        json.dump(result, f, indent=20)
+        
+    return result
